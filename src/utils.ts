303e6fe464924efb740908090c31198c8ff2af3d
Lete114 <me@imlete.cn>
2024-04-29 18:46:56 +0800

    fix: crypto has no randomUUID function

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
 export function getUUID() {
-  if (crypto) { return crypto.randomUUID() }
+  if (crypto && crypto.randomUUID) { return crypto.randomUUID() }
   return URL.createObjectURL(new Blob(['1'])).split('/').pop()!
 }
